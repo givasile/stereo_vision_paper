15762e121d0adf7143a2cb36250f92b94d9fa57b
Vasilis Gkolemis <ntipakos@gmail.com>
2020-02-14 17:34:39 +0000

    updated msnet scales evaluation figure

--- paper/create_MSNet_scales_evaluation.py
+++ paper/create_MSNet_scales_evaluation.py
@@ -37,49 +37,49 @@ for i, exp in enumerate(experiments):
 results = np.array(results)
 
 # plot
-fig, ax = plt.subplots(2, 2, sharex='all')
+fig, ax = plt.subplots(1, 2, sharex='all')
 fig.suptitle("MSNet evaluation using different combinations of scales")
 
 # first row of plots
-ax[0][0].plot(results[:,0], 'ro')
-ax[0][0].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
-ax[0][0].axvspan(3.5, 9.5, alpha=0.5, color='green')
-ax[0][0].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
-ax[0][0].axvspan(13.5, 14.5, alpha=0.5, color='red')
-ax[0][0].set_ylabel('$\mu$ Absolute Error')
+ax[0].plot(results[:,0], 'ro')
+ax[0].axvspan(-0.5, 3.5, alpha=0.5, color='gray')
+ax[0].axvspan(3.5, 9.5, alpha=0.5, color='darkgrey')
+ax[0].axvspan(9.5, 13.5, alpha=0.5, color='silver')
+ax[0].axvspan(13.5, 14.5, alpha=0.5, color='lightgrey')
+ax[0].set_ylabel('$\mu$ Absolute Error')
 
-ax[0][1].plot(results[:,1], 'ro')
-ax[0][1].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
-ax[0][1].axvspan(3.5, 9.5, alpha=0.5, color='green')
-ax[0][1].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
-ax[0][1].axvspan(13.5, 14.5, alpha=0.5, color='red')
-ax[0][1].set_ylabel('$\sigma$ Absolute Error')
+ax[1].plot(results[:,1], 'ro')
+ax[1].axvspan(-0.5, 3.5, alpha=0.5, color='gray')
+ax[1].axvspan(3.5, 9.5, alpha=0.5, color='darkgrey')
+ax[1].axvspan(9.5, 13.5, alpha=0.5, color='silver')
+ax[1].axvspan(13.5, 14.5, alpha=0.5, color='lightgrey')
+ax[1].set_ylabel('$\sigma$ Absolute Error')
 
 # second row of plots
-ax[1][0].plot(results[:,2], 'ro')
-ax[1][0].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
-ax[1][0].axvspan(3.5, 9.5, alpha=0.5, color='green')
-ax[1][0].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
-ax[1][0].axvspan(13.5, 14.5, alpha=0.5, color='red')
-ax[1][0].set_ylabel('$\mu$ PCG Error')
+# ax[1][0].plot(results[:,2], 'ro')
+# ax[1][0].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
+# ax[1][0].axvspan(3.5, 9.5, alpha=0.5, color='green')
+# ax[1][0].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
+# ax[1][0].axvspan(13.5, 14.5, alpha=0.5, color='red')
+# ax[1][0].set_ylabel('$\mu$ PCG Error')
 
-ax[1][1].plot(results[:,3], 'ro')
-ax[1][1].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
-ax[1][1].axvspan(3.5, 9.5, alpha=0.5, color='green')
-ax[1][1].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
-ax[1][1].axvspan(13.5, 14.5, alpha=0.5, color='red')
-ax[1][1].set_ylabel('$\sigma$ PCG Error')
+# ax[1][1].plot(results[:,3], 'ro')
+# ax[1][1].axvspan(-0.5, 3.5, alpha=0.5, color='blue')
+# ax[1][1].axvspan(3.5, 9.5, alpha=0.5, color='green')
+# ax[1][1].axvspan(9.5, 13.5, alpha=0.5, color='magenta')
+# ax[1][1].axvspan(13.5, 14.5, alpha=0.5, color='red')
+# ax[1][1].set_ylabel('$\sigma$ PCG Error')
 
 # set x labels
 xnames = ['\{4\}', '\{8\}', '\{16\}', '\{32\}',
           '\{4,8\}', '\{4,16\}', '\{4,32\}', '\{8,16\}', '\{8, 32\}', '\{16,32\}',
           '\{4,8,16\}','\{4,8,32\}','\{4,16,32\}','\{8,16,32\}',
           '\{4,8,16,32\}']
-ax[0][0].set_xticks(range(len(results[:,3])))
-ax[0][0].set_xticklabels(xnames, rotation=80)
-ax[0][1].set_xticklabels(xnames, rotation=80)
-ax[1][0].set_xticklabels(xnames, rotation=80)
-ax[1][1].set_xticklabels(xnames, rotation=80)
+ax[0].set_xticks(range(len(results[:,3])))
+ax[0].set_xticklabels(xnames, rotation=80)
+ax[1].set_xticklabels(xnames, rotation=80)
+# ax[1][0].set_xticklabels(xnames, rotation=80)
+# ax[1][1].set_xticklabels(xnames, rotation=80)
 
 fig.savefig(os.path.join(dir_latex_figures, fig_name), bbox_inches='tight')
 plt.show(block=False)
